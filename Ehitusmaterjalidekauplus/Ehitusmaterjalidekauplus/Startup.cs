7393b3b54795181e9980a8f07a11e8273864b6be
opilane <opilane@HP-CZC6297B8B>
2019-06-04 12:44:02 +0300

    Loon projekti

---
+++ Ehitusmaterjalidekauplus/Ehitusmaterjalidekauplus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ehitusmaterjalidekauplus.Startup))]
+namespace Ehitusmaterjalidekauplus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

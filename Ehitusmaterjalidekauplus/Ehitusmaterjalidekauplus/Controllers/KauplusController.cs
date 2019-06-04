b98e564a42eb048f66167aa0c6a80b73ad546c75
opilane <opilane@HP-CZC6297B8B>
2019-06-04 12:52:21 +0300

    Ten Kaupluse kontrolleri

---
+++ Ehitusmaterjalidekauplus/Ehitusmaterjalidekauplus/Controllers/KauplusController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ehitusmaterjalidekauplus.Controllers
+{
+    public class KauplusController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Kauplus
+        public ActionResult Index()
+        {
+            return View(db.Kauplus.ToList());
+        }
+    }
+}
